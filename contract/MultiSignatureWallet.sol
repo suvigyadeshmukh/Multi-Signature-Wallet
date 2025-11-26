5b4f556dc15de47a3955cd407d7d1b78292751f3
Suvigya Deshmukh <suvigyadeshmukh@gmail.com>
2025-11-26 15:56:36 +0530

    Update MultiSignatureWallet.sol

--- contract/MultiSignatureWallet.sol
+++ contract/MultiSignatureWallet.sol
@@ -1,1 +1,219 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+/**
+ * @title Multi-Signature Wallet
+ * @dev A secure wallet that requires multiple owner approvals for transactions
+ * @notice This version has hardcoded owners for easy deployment
+ */
+contract Project {
+    
+    // Events
+    event Deposit(address indexed sender, uint amount, uint balance);
+    event SubmitTransaction(
+        address indexed owner,
+        uint indexed txIndex,
+        address indexed to,
+        uint value,
+        bytes data
+    );
+    event ConfirmTransaction(address indexed owner, uint indexed txIndex);
+    event ExecuteTransaction(address indexed owner, uint indexed txIndex);
+
+    // State variables
+    address[] public owners;
+    mapping(address => bool) public isOwner;
+    uint public numConfirmationsRequired;
+
+    struct Transaction {
+        address to;
+        uint value;
+        bytes data;
+        bool executed;
+        uint numConfirmations;
+    }
+
+    mapping(uint => mapping(address => bool)) public isConfirmed;
+    Transaction[] public transactions;
+
+    // Modifiers
+    modifier onlyOwner() {
+        require(isOwner[msg.sender], "Not an owner");
+        _;
+    }
+
+    modifier txExists(uint _txIndex) {
+        require(_txIndex < transactions.length, "Transaction does not exist");
+        _;
+    }
+
+    modifier notExecuted(uint _txIndex) {
+        require(!transactions[_txIndex].executed, "Transaction already executed");
+        _;
+    }
+
+    modifier notConfirmed(uint _txIndex) {
+        require(!isConfirmed[_txIndex][msg.sender], "Transaction already confirmed");
+        _;
+    }
+
+    /**
+     * @dev Constructor automatically sets the deployer as the first owner
+     * @notice Edit the owners array below to add your own addresses
+     */
+    constructor() {
+        // CONFIGURE YOUR WALLET HERE:
+        // Replace these addresses with your actual wallet addresses
+        address[3] memory _owners = [
+            msg.sender,  // Deployer's address (automatically set)
+            0x5B38Da6a701c568545dCfcB03FcB875f56beddC4,  // Replace with owner 2
+            0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2   // Replace with owner 3
+        ];
+        
+        // Number of confirmations required (2 out of 3 in this case)
+        uint _numConfirmationsRequired = 2;
+
+        require(_owners.length > 0, "Owners required");
+        require(
+            _numConfirmationsRequired > 0 &&
+                _numConfirmationsRequired <= _owners.length,
+            "Invalid number of required confirmations"
+        );
+
+        for (uint i = 0; i < _owners.length; i++) {
+            address owner = _owners[i];
+
+            require(owner != address(0), "Invalid owner");
+            require(!isOwner[owner], "Owner not unique");
+
+            isOwner[owner] = true;
+            owners.push(owner);
+        }
+
+        numConfirmationsRequired = _numConfirmationsRequired;
+    }
+
+    /**
+     * @dev Allows contract to receive Ether
+     */
+    receive() external payable {
+        emit Deposit(msg.sender, msg.value, address(this).balance);
+    }
+
+    /**
+     * @dev Core Function 1: Submit a new transaction
+     * @param _to Destination address
+     * @param _value Amount of Ether to send (in wei)
+     * @param _data Transaction data (use "0x" for simple transfers)
+     */
+    function submitTransaction(
+        address _to,
+        uint _value,
+        bytes memory _data
+    ) public onlyOwner {
+        uint txIndex = transactions.length;
+
+        transactions.push(
+            Transaction({
+                to: _to,
+                value: _value,
+                data: _data,
+                executed: false,
+                numConfirmations: 0
+            })
+        );
+
+        emit SubmitTransaction(msg.sender, txIndex, _to, _value, _data);
+    }
+
+    /**
+     * @dev Core Function 2: Confirm a pending transaction
+     * @param _txIndex Transaction index to confirm
+     */
+    function confirmTransaction(uint _txIndex)
+        public
+        onlyOwner
+        txExists(_txIndex)
+        notExecuted(_txIndex)
+        notConfirmed(_txIndex)
+    {
+        Transaction storage transaction = transactions[_txIndex];
+        transaction.numConfirmations += 1;
+        isConfirmed[_txIndex][msg.sender] = true;
+
+        emit ConfirmTransaction(msg.sender, _txIndex);
+    }
+
+    /**
+     * @dev Core Function 3: Execute a confirmed transaction
+     * @param _txIndex Transaction index to execute
+     */
+    function executeTransaction(uint _txIndex)
+        public
+        onlyOwner
+        txExists(_txIndex)
+        notExecuted(_txIndex)
+    {
+        Transaction storage transaction = transactions[_txIndex];
+
+        require(
+            transaction.numConfirmations >= numConfirmationsRequired,
+            "Cannot execute: not enough confirmations"
+        );
+
+        transaction.executed = true;
+
+        (bool success, ) = transaction.to.call{value: transaction.value}(
+            transaction.data
+        );
+        require(success, "Transaction failed");
+
+        emit ExecuteTransaction(msg.sender, _txIndex);
+    }
+
+    /**
+     * @dev Get the total number of transactions
+     */
+    function getTransactionCount() public view returns (uint) {
+        return transactions.length;
+    }
+
+    /**
+     * @dev Get transaction details
+     */
+    function getTransaction(uint _txIndex)
+        public
+        view
+        returns (
+            address to,
+            uint value,
+            bytes memory data,
+            bool executed,
+            uint numConfirmations
+        )
+    {
+        Transaction storage transaction = transactions[_txIndex];
+
+        return (
+            transaction.to,
+            transaction.value,
+            transaction.data,
+            transaction.executed,
+            transaction.numConfirmations
+        );
+    }
+
+    /**
+     * @dev Get list of owners
+     */
+    function getOwners() public view returns (address[] memory) {
+        return owners;
+    }
+
+    /**
+     * @dev Get contract balance
+     */
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+}
